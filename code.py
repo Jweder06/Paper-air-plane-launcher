83fac47642ecbf294216ae64fdde10a5a4e870de
Jweder06 <jweder06@charlottesvilleschools.org>
2023-06-01 10:27:02 -0400

    Code update

--- code.py
+++ code.py
@@ -1,5 +1,22 @@
+#type: ignore
 from time import sleep
+import time
+import math
+import board
+import digitalio
+from digitalio import DigitalInOut, Direction, Pull
+from pwmio import PWMOut
+from adafruit_motor import servo
+Servo1 = servo.Servo(pwmio.PWMOut(board.D1 , duty_cycle=2 ** 15, frequency=50))
+Servo2 = servo.Servo(pwmio.PWMOut(board.D2, duty_cycle=2 ** 15, frequency=50))
+while True:
+    Servo2.angle = 0
+    Servo1.angle = 0
+    time.sleep(1)
+    Servo2.angle = 180
+    Servo1.angle = 180
+    time.sleep(1)
 
 while True:
-    print("Hello world!")
-    sleep(1)
+#    Servo1.angle = 70
+#    Servo2.angle = 70
